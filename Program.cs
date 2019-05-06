45acb1537a5f6b6adee2aacfb16a6d71cbf17c67
liam-ash <liam.ash@xero.com>
2019-05-06 10:20:44 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ACMG_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //test
+        }
+    }
+}
